6f5e623fbf41b1db0cb2fe5642dd91ad99fc8008
3laaHisham <alaa.ismail@student.guc.edu.eg>
2022-12-09 00:40:42 +0200

    ✅ made TestAddingNNumbersDiffDelimiters pass

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -3,14 +3,13 @@ import java.util.Arrays;
 public class SimpleCalculator {
 
     int add(String numbers){
-        char delimiter = ',';
+        String delimiters = ",\n";
         if (numbers.charAt(0) == '/') {
-            delimiter = numbers.charAt(2);
+            delimiters += numbers.charAt(2);
             numbers = numbers.substring(4);
         }
 
-        String[] strArr = numbers.split("[,\n]|[" + delimiter +"]");
-        System.out.println(Arrays.toString(strArr));
+        String[] strArr = numbers.split("[" + delimiters +"]");
         Object[] intArr = Arrays.stream(strArr).map((x) -> Integer.parseInt(x)).toArray();
 
         int res = (int) Arrays.stream(intArr).reduce(0, (x, y) -> (int) x + (int) y);
