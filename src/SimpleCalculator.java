22f42dc144ae49bc6e5818b9d86bf48904dc1839
3laaHisham <alaa.ismail@student.guc.edu.eg>
2022-12-09 01:57:37 +0200

    ✅ made TestAddingNNumbersDelimiterNLength pass

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -4,24 +4,35 @@ public class SimpleCalculator {
 
     int add(String numbers) throws Exception{
 
-        String delimiters = ",\n";
-        if (numbers.charAt(0) == '/') {
-            delimiters += numbers.charAt(2);
-            numbers = numbers.substring(4);
-        }
+        String delimiters = ",|\n";
+        if (numbers.charAt(0) == '/')
+            if (numbers.contains("[")) {
+                delimiters += "|" + numbers.substring(3, numbers.indexOf("]"));
+                numbers = numbers.substring(numbers.indexOf("]") + 2);
+            }
+            else {
+                delimiters += numbers.charAt(2);
+                numbers = numbers.substring(4);
+            }
 
-        String[] strArr = numbers.split("[" + delimiters +"]");
-        Object[] intArr = Arrays.stream(strArr).map(Integer::parseInt).toArray();
-
-        for (int i = 0; i < intArr.length; i++) {
-            int num = (int) intArr[i];
-            if (num < 0)
+        String[] strArr = numbers.split("[" + delimiters + "]");
+        Object[] intArr = Arrays.stream(strArr).map(SimpleCalculator::parseString).toArray();
+        for (Object num : intArr)
+            if ((int) num < 0)
                 throw new Exception();
-            if (num > 1000)
-                intArr[i] = 0;
-        }
 
         int res = (int) Arrays.stream(intArr).reduce(0, (x, y) -> (int) x + (int) y);
         return res;
+    }
+
+    static int parseString(Object x){
+        if (x == "")
+            return 0;
+
+        int y = Integer.parseInt((String) x);
+        if (y > 1000)
+            return 0;
+
+        return y;
     }
 }
