18d0972bed8e5343f1d411db52730992ce1bd366
3laaHisham <alaa.ismail@student.guc.edu.eg>
2022-12-08 23:57:33 +0200

    🔄 refactored AddingNNumbers

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -4,12 +4,9 @@ public class SimpleCalculator {
 
     int add(String numbers){
         String[] strArr = numbers.split(",");
-//        Object[] intArr = Arrays.stream(strArr).map((x) -> Integer.parseInt(x)).toArray();
-        int[] intArr = new int[strArr.length];
-        for (int i = 0; i < strArr.length; i++)
-            intArr[i] = Integer.parseInt(strArr[i]);
+        Object[] intArr = Arrays.stream(strArr).map((x) -> Integer.parseInt(x)).toArray();
 
-        int res = Arrays.stream(intArr).reduce(0, (x, y) -> x + y);
+        int res = (int) Arrays.stream(intArr).reduce(0, (x, y) -> (int) x + (int) y);
         return res;
     }
 }
