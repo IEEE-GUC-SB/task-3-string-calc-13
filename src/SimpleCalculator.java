da560f798b158da8e626bd90a6ee531ea4c0c440
3laaHisham <alaa.ismail@student.guc.edu.eg>
2022-12-08 23:55:28 +0200

    ✅ made TestAddingNNumbers Pass

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
+
 public class SimpleCalculator {
 
     int add(String numbers){
-        String[] intArr = numbers.split(",");
-        int fNumber = Character.getNumericValue(numbers.charAt(0));
-        int sNumber = Character.getNumericValue(numbers.charAt(2));
-        int res = fNumber + sNumber;
+        String[] strArr = numbers.split(",");
+//        Object[] intArr = Arrays.stream(strArr).map((x) -> Integer.parseInt(x)).toArray();
+        int[] intArr = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++)
+            intArr[i] = Integer.parseInt(strArr[i]);
+
+        int res = Arrays.stream(intArr).reduce(0, (x, y) -> x + y);
         return res;
     }
 }
