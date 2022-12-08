7437fa58d31752dd5f8320fa53d1bf98db2c31ab
3laaHisham <alaa.ismail@student.guc.edu.eg>
2022-12-09 00:16:59 +0200

    ❌ failing TestAddingNNumbersDiffDelimiters

--- Tests/SimpleCalculatorTest.java
+++ Tests/SimpleCalculatorTest.java
@@ -20,9 +20,16 @@ public class SimpleCalculatorTest {
         int res = calculator.add("6,2,4,1");
         assertEquals(13, res);
     }
+
     @Test
     void TestAddingNNumbersNewLine(){
         int res = calculator.add("1\n2,3\n5");
+        assertEquals(11, res);
+    }
+
+    @Test
+    void TestAddingNNumbersDiffDelimiters(){
+        int res = calculator.add("//;\\n1;2;3;5");
         assertEquals(11, res);
     }
 }
