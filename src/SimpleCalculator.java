6bb063d8eaa596ba3e76613a5478f5677ecf583e
3laaHisham <alaa.ismail@student.guc.edu.eg>
2022-12-09 13:54:06 +0200

    🔄 refactored AddingNNumbersDelimiterNLength

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -2,37 +2,43 @@ import java.util.Arrays;
 
 public class SimpleCalculator {
 
-    int add(String numbers) throws Exception{
+    int add(String numbers){
+        String delimiters = fetchDelimiters(numbers);
+        String numbersExpression = fetchNumbersExpression(numbers);
 
-        String delimiters = ",|\n";
-        if (numbers.charAt(0) == '/')
-            if (numbers.contains("[")) {
-                delimiters += "|" + numbers.substring(3, numbers.indexOf("]"));
-                numbers = numbers.substring(numbers.indexOf("]") + 2);
-            }
-            else {
-                delimiters += numbers.charAt(2);
-                numbers = numbers.substring(4);
-            }
-
-        String[] strArr = numbers.split("[" + delimiters + "]");
-        Object[] intArr = Arrays.stream(strArr).map(SimpleCalculator::parseString).toArray();
-        for (Object num : intArr)
-            if ((int) num < 0)
-                throw new Exception();
+        String[] strArr = numbersExpression.split("[" + delimiters + "]");
+        Object[] intArr = Arrays.stream(strArr).map(this::parseInt).toArray();
 
         int res = (int) Arrays.stream(intArr).reduce(0, (x, y) -> (int) x + (int) y);
         return res;
     }
 
-    static int parseString(Object x){
+    String fetchDelimiters (String numbers) {
+        String delimiters = ",|\n";
+        if (numbers.charAt(0) == '/')
+            if (numbers.contains("["))
+                delimiters += numbers.substring(numbers.indexOf("[") + 1, numbers.indexOf("]"));
+            else
+                delimiters += numbers.charAt(2);
+
+        return delimiters;
+    }
+
+    String fetchNumbersExpression (String numbers) {
+        if (numbers.charAt(0) == '/')
+            numbers = numbers.substring(numbers.indexOf("\n") + 1);
+        return numbers;
+    }
+
+    int parseInt (Object x) throws RuntimeException {
         if (x == "")
             return 0;
 
         int y = Integer.parseInt((String) x);
         if (y > 1000)
             return 0;
-
+        if (y < 0 )
+            throw new RuntimeException();
         return y;
     }
 }
